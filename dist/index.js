6e770b6c22bed664b05810ce42d88b114a53b6c0
jacob <pralhadkharel@gmail.com>
2024-01-20 14:20:58 +0545

    setup

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,13 @@ const express_1 = __importDefault(require("express"));
 // import sampleRoute from './router/index.js';
 const index_1 = __importDefault(require("./router/index"));
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 // Apply middleware globally
 app.use(express_1.default.json());
 // Use the sample route
 app.use('/api', index_1.default);
+console.log(1);
+console.log(1);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ import express from 'express';
 import sampleRoute from './router/index';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Apply middleware globally
 app.use(express.json());
 
 // Use the sample route
-app.use('/api', sampleRoute);
+app.use('/api', sampleRoute); 
+console.log(1)
+console.log(1)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+ 
